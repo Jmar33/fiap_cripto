899748169c1ae5e04e1c04425439ccf5a3d5a970
Josimar Ferreira <58815497+Jmar33@users.noreply.github.com>
2023-11-20 18:04:42 -0300

    Delete persistences_types_3aojr-master directory

--- persistences_types_3aojr-master/android/app/src/main/kotlin/com/example/persistences_types_3aojr/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.persistences_types_3aojr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- persistences_types_3aojr-master/lib/types/menu.dart
+++
@@ -1,8 +0,0 @@
-class Menu {
-  final String title;
-  final String subtitle;
-  final String icon;
-  final String path;
-
-  Menu(this.title, this.subtitle, this.icon, this.path);
-}
